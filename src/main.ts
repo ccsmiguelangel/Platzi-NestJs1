accf19c8401b28e3f1593cd93a2edc4f9900712b
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-01-14 22:27:24 -0400

    finalizando curso

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // ignora publicar elementos sin los datos que se indicaron en dtos
+    forbidNonWhitelisted: true, // alerta por qué los datos no se aceptan
+  }));
   await app.listen(3000);
 }
 bootstrap();
